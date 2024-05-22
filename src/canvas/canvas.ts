6f73ae7fa1f15e7c804b7c8923b94133eaf505be
Arsen Abrahamyan <arsjj7@gmail.com>
2024-05-22 23:05:03 +0400

    fix losing functionality of controls beacuse of code spliting

--- src/canvas/canvas.ts
+++ src/canvas/canvas.ts
@@ -1,4 +1,5 @@
 import drawPlayground from "./playground";
+import { getRandomColor } from "../utils";
 
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
@@ -32,7 +33,8 @@ dxControl?.addEventListener("input", () => {
   dxValue = parseFloat(dxControl.value);
 });
 
-dyControl?.addEventListener("input", () => {
+dyControl && dyControl.addEventListener("input", () => {
+  console.log(1)
   dyValue = parseFloat(dyControl.value);
 });
 
@@ -187,14 +189,7 @@ export function initializeUserEvents() {
   tick();
 }
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+
 
 function tick() {
   if (!ctx) return;
